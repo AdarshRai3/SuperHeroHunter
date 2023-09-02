57164f2b04acbf58b53b41a9e2908334a59f46f3
AdarshRai3 <git config --global user.email>
2023-09-02 20:26:17 +0530

    h1

--- hash.js
+++ hash.js
@@ -1,8 +1,8 @@
 const crypto = require("crypto-js");
 
-function getMD5Hash(text) {
-  const hash = crypto.MD5(text);
+function getMD5Hash(ts, privateKey, publicKey) {
+  const hash = crypto.MD5(ts + privateKey + publicKey);
   return hash.toString();
 }
 
-console.log(getMD5Hash("text to hash"));
+console.log(getMD5Hash("1654237600", "63d44a2ff474121a6c8cce8ea9fc3cc05aac4c74", "801ffb179c43e62422d9c6ac3f1203f0"));
