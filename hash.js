365503cd66b9face4c80126279291dba9d8c8e69
AdarshRai3 <git config --global user.email>
2023-09-05 19:48:31 +0530

    final commit 1

--- hash.js
+++ hash.js
@@ -1,8 +1,16 @@
-const crypto = require("crypto-js");
+var MD5 = require("crypto-js/md5");
 
-function getMD5Hash(ts, privateKey, publicKey) {
-  const hash = crypto.MD5(ts + privateKey + publicKey);
-  return hash.toString();
-}
+// Set your public and private keys
+var publicKey = "801ffb179c43e62422d9c6ac3f1203f0";
+var privateKey = "63d44a2ff474121a6c8cce8ea9fc3cc05aac4c74";
 
-console.log(getMD5Hash("1654237600", "63d44a2ff474121a6c8cce8ea9fc3cc05aac4c74", "801ffb179c43e62422d9c6ac3f1203f0"));
+// Generate a timestamp
+var ts = new Date().getTime();
+
+// Create a string to hash
+var toHash = ts + privateKey + publicKey;
+
+// Generate the hash
+var hashVal = MD5(toHash).toString();
+
+module.exports = { ts, publicKey, hashVal };
