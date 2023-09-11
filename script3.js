1932d731b9fb86121ffb4346d6e7a899c49167d4
AdarshRai3 <git config --global user.email>
2023-09-11 11:13:03 +0530

    21th

--- script3.js
+++ script3.js
@@ -1,76 +1,3 @@
-let input = document.getElementById("input-box");
-let button = document.getElementById("submit-button");
-let showContainer = document.getElementById("show-container");
-let listContainer = document.querySelector(".list");
-
-const [timestamp, apiKey, hashValue] = [ts, publicKey, hashVal];
-
-function displayWords(value) {
-  input.value = value;
-  removeElements();
-}
-
-function removeElements() {
-  listContainer.innerHTML = "";
-}
-
-input.addEventListener("keyup", async () => {
-  removeElements();
-  if (input.value.length < 4) {
-    return false;
-  }
-
-  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&nameStartsWith=${input.value}`;
-
-  const response = await fetch(url);
-  const jsonData = await response.json();
-
-  if(jsonData.data) {
-    jsonData.data["results"].forEach((result) => {
-      let name = result.name;
-      let div = document.createElement("div");
-      div.style.cursor = "pointer";
-      div.classList.add("autocomplete-items");
-      div.setAttribute("onclick", "displayWords('" + name + "')");
-      let word = "<b>" + name.substr(0, input.value.length) + "</b>";
-      word += name.substr(input.value.length);
-      div.innerHTML = `<p class="item">${word}</p>`;
-      listContainer.appendChild(div);
-    });
-  }
-});
-
-button.addEventListener(
-  "click",
-  (getRsult = async () => {
-    if (input.value.trim().length < 1) {
-      alert("Input cannot be blank");
-    }
-    showContainer.innerHTML = "";
-    const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
-
-    const response = await fetch(url);
-    const jsonData = await response.json();
-    
-    if(jsonData.data) {
-      jsonData.data["results"].forEach((element) => {
-        showContainer.innerHTML += `<div class="card-container">
-          <div class="container-character-image">
-          <img src="${
-            element.thumbnail["path"] + "." + element.thumbnail["extension"]
-          }"/></div>
-          <div class="character-name">${element.name}</div>
-          <div class="character-description">${element.description}</div>
-          </div>`;
-      });
-    }
-  })
-);
-
-window.onload = () => {
-  getRsult();
-};
-//function for show heros depends on search also filter heros depends on key press
 const showCorrespondingHeros = () => {
   const xhr = new XMLHttpRequest();
   const name = document.getElementById("name").value;
@@ -110,7 +37,7 @@ const showCorrespondingHeros = () => {
 
       xhr.send();
 }
-
+  
 // make a favourites key for storing all favourites hero's id in local storage if not available
 if (localStorage.getItem("favourites")==null) {
   localStorage.setItem("favourites",JSON.stringify([]));
