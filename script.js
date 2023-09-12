97c145e544ff0cb085d29546ae925e5794f1bbb2
AdarshRai3 <git config --global user.email>
2023-09-12 17:41:55 +0530

    56

--- script.js
+++ script.js
@@ -42,25 +42,23 @@ input.addEventListener("keyup", async () => {
 });
 
 const getRsult = async () => {
-  if (input.value.trim().length < 1) {
-    alert("Input cannot be blank");
-  }
-  showContainer.innerHTML = "";
-  const url = `https://gateway.marvel.com:443/v1/public/characters?ts=${timestamp}&apikey=${apiKey}&hash=${hashValue}&name=${input.value}`;
-
-  const response = await fetch(url);
-  const jsonData = await response.json();
-  jsonData.data["results"].forEach((element) => {
-    showContainer.innerHTML = `<div class="card-container">
-        <div class="container-character-image">
-        <img src="${
-          element.thumbnail["path"] + "." + element.thumbnail["extension"]
-        }"/></div>
-        <div class="character-name">${element.name}</div>
-        <div class="character-description">${element.description}</div>
-        </div>`;
-  });
-};
+    const jsonData = await fetch(url).then(response => response.json());
+    if (input.value.trim().length < 1) {
+      alert("Input cannot be blank");
+    }
+    showContainer.innerHTML = "";
+    jsonData.data["results"].forEach((element) => {
+      showContainer.innerHTML = `<div class="card-container">
+          <div class="container-character-image">
+          <img src="${
+            element.thumbnail["path"] + "." + element.thumbnail["extension"]
+          }"/></div>
+          <div class="character-name">${element.name}</div>
+          <div class="character-description">${element.description}</div>
+          </div>`;
+    });
+  };
+  
 
 window.onload = () => {
   getRsult();
