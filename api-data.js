6fc55da6bf847da2b57d9198bd4cd41a37a0ea13
AdarshRai3 <git config --global user.email>
2023-09-20 19:48:41 +0530

    commit-3

--- api-data.js
+++ api-data.js
@@ -1,3 +1,27 @@
-let ts = "1681802982683";
-let publicKey = "801ffb179c43e62422d9c6ac3f1203f0";
-let hashVal = "80480af9d0b60bf9633ae2fd36ecd4f9";
+const crypto = require('crypto');
+
+function getDateTime() {
+    let currentdate = new Date(); 
+    let datetime = currentdate.getDate() + 
+                + (currentdate.getMonth()+1)  + 
+                + currentdate.getFullYear() +  
+                + currentdate.getHours() + 
+                + currentdate.getMinutes() + 
+                + currentdate.getSeconds();
+    return datetime;
+}
+let ts = getDateTime();
+let public_key = "801ffb179c43e62422d9c6ac3f1203f0";
+let private_key = "63d44a2ff474121a6c8cce8ea9fc3cc05aac4c74";
+
+
+
+function createHash(ts, public_key, private_key) {
+    const hash = crypto.createHash('md5');
+    hash.update(ts + private_key + public_key);
+    return hash.digest('hex');
+}
+
+
+
+let hash_value = createHash(ts, public_key, private_key);
