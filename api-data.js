868905fc067f816a27f173030d9155ffe824d3a9
AdarshRai3 <git config --global user.email>
2023-09-20 19:54:32 +0530

    Xommit

--- api-data.js
+++ api-data.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 function getDateTime() {
     let currentdate = new Date(); 
     let datetime = currentdate.getDate() + 
-                + (currentdate.getMonth()+1)  + 
+                + currentdate.getMonth()+ 
                 + currentdate.getFullYear() +  
                 + currentdate.getHours() + 
                 + currentdate.getMinutes() + 
@@ -11,17 +11,19 @@ function getDateTime() {
     return datetime;
 }
 let ts = getDateTime();
-let public_key = "801ffb179c43e62422d9c6ac3f1203f0";
+let publicKey = "801ffb179c43e62422d9c6ac3f1203f0";
 let private_key = "63d44a2ff474121a6c8cce8ea9fc3cc05aac4c74";
 
 
 
-function createHash(ts, public_key, private_key) {
+function createHash(ts, publickey, private_key) {
     const hash = crypto.createHash('md5');
-    hash.update(ts + private_key + public_key);
+    hash.update(ts + private_key + publickey);
     return hash.digest('hex');
 }
 
 
 
-let hash_value = createHash(ts, public_key, private_key);
+let hash_Val = createHash(ts, publickey, private_key);
+
+let hashVal=hash_Val;
